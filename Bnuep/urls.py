19c9e9c4a96c75c8089facbb738dbfa2bf5ca362
杉菜 <dickqi87@gmail.com>
2019-10-24 19:24:31 +0800

    add static root setting

--- Bnuep/urls.py
+++ Bnuep/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('app.account.urls', namespace='account')),
     path('contest/', include('app.contest.urls', namespace='contest')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
